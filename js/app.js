8105b828dc4c17c39cde2d6b85bf953dfc76a6b7
Joseph Ortiz <josepho011235@gmail.com>
2014-08-24 17:25:42 -0400

    added ryu cool feature

--- js/app.js
+++ js/app.js
@@ -1,34 +1,54 @@
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-  .mousedown(function() {
-    playHadouken();    
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show()
-    .animate(
-      {'left': '300px'},
-      500,
-      function() {
-        $(this).hide();
-        $(this).css('left', '-212px');
-      }
-    );
-  })
-  .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-  });
+    
+
+
+    $('.ryu').mouseenter(function() {
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+        .mouseleave(function() {
+            $('.ryu-ready').hide();
+            $('.ryu-still').show();
+        })
+        .mousedown(function() {
+            playHadouken();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').show();
+            $('.hadouken').finish().show()
+                .animate({
+                        'left': '300px'
+                    },
+                    500,
+                    function() {
+                        $(this).hide();
+                        $(this).css('left', '-212px');
+                    }
+            );
+        })
+        .mouseup(function() {
+            $('.ryu-throwing').hide();
+            $('.ryu-ready').show();
+        });
+
+
+ $(document).keydown(function() {
+        //alert("change style");
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').show();
+
+    }).keyup(function(){
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+
+    });
+    
+
 });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
 }
